8e8cb9946391c4319c7007e52bc2e0d80c2458dd
funzza <netraluis@gmail.com>
2021-05-04 23:42:33 +0200

    04-05-2021--23:42

--- src/components/google-maps/GoogleMaps.js
+++ src/components/google-maps/GoogleMaps.js
@@ -5,10 +5,10 @@ const GoogleMaps = () => (
     <iframe
       width="100%"
       height="400"
-      frameborder="0"
+      frameBorder="0"
       scrolling="no"
-      marginheight="0"
-      marginwidth="0"
+      marginHeight="0"
+      marginWidth="0"
       src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=es&amp;q=Passeig%20de%20Sant%20D%C3%ADdac,%2025%20sitges%20Barcelona,%20Spain+(Mi%20nombre%20de%20egocios)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
     ></iframe>
   </div>
--- src/components/stripe/card-form-element.js
+++ src/components/stripe/card-form-element.js
@@ -2,7 +2,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from '@stripe/stripe-js';
 import CardForm from './card-form';
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
+const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_KEY}`);
 
 const cardFormElement = (props) => {
     return (
